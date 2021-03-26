8d89266376d981dbae6d60a7c0f82247c90ab8ac
David Kajpust <kajpustd@gmail.com>
2021-03-26 17:16:36 -0400

    Add in the mainnet subgraph. Only with the council multisig

--- src/mappings/gnosisMultisig.ts
+++ src/mappings/gnosisMultisig.ts
@@ -9,17 +9,18 @@ import {
 import { Council } from "../types/schema";
 
 export function handleAddedOwner(event: AddedOwner): void {
-  let council = createOrLoadCouncil();
-
+  let council = createOrLoadCouncil(event.address);
   let members = council.members;
-  members.push(event.params.owner);
+
+  let index = members.indexOf(event.params.owner);
+  if (index == -1) members.push(event.params.owner);
 
   council.members = members;
   council.save();
 }
 
 export function handleRemovedOwner(event: RemovedOwner): void {
-  let council = createOrLoadCouncil();
+  let council = createOrLoadCouncil(event.address);
   let members = council.members;
 
   let index = members.indexOf(event.params.owner);
@@ -30,18 +31,16 @@ export function handleRemovedOwner(event: RemovedOwner): void {
 }
 
 export function handleChangedThreshold(event: ChangedThreshold): void {
-  let council = createOrLoadCouncil();
+  let council = createOrLoadCouncil(event.address);
   council.threshold = event.params.threshold;
   council.save();
 }
 
-function createOrLoadCouncil(): Council {
+function createOrLoadCouncil(multisigAddress: Address): Council {
   let council = Council.load("1");
   if (council == null) {
     council = new Council("1");
-    let contract = GnosisMultisig.bind(
-      Address.fromString("0x1679A1D1caf1252BA43Fb8Fc17ebF914a0C725AE")
-    );
+    let contract = GnosisMultisig.bind(multisigAddress);
     let members: Array<Bytes> = [];
     let membersAsAddress = contract.getOwners();
     for (let i = 0; i < membersAsAddress.length; i++) {
